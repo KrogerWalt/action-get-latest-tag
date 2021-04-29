05acb06dc5b68f8f753fda7206c2a74e344c4760
Walt Moorhouse <60402633+KrogerWalt@users.noreply.github.com>
2021-04-29 10:20:26 -0700

    Trying to prevent grabbing old labels
    Original action sometimes gets the next to last label, which breaks the build.  Trying to fix that.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,9 @@ git fetch --tags
 # This suppress an error occurred when the repository is a complete one.
 git fetch --prune --unshallow || true
 
-latest_tag=''
 
-if [ "${INPUT_SEMVER_ONLY}" = 'false' ]; then
-  # Get a actual latest tag.
-  latest_tag=$(git describe --abbrev=0 --tags)
-else
-  # Get a latest tag in the shape of semver.
-  for ref in $(git for-each-ref --sort=-creatordate --format '%(refname)' refs/tags); do
-    tag="${ref#refs/tags/}"
-    if echo "${tag}" | grep -Eq '^v?([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?$'; then
-      latest_tag="${tag}"
-      break
-    fi
-  done
-fi
+latest_tag=$(git tag -l --sort=taggerdate | sed '$!d')
+
 
 if [ "${latest_tag}" = '' ] && [ "${INPUT_WITH_INITIAL_VERSION}" = 'true' ]; then
   latest_tag="${INPUT_INITIAL_VERSION}"
